317feb5e9eb382dba1f174baed2e1107fa2f2665
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-23 19:29:15 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,7 +16,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, SUM(pledges.amount) AS sum_of_pledges FROM projects INNER JOIN pledges ON projects.id = pledges.user_id GROUP BY users.name ORDER BY sum_of_pledges ASC;"
+"SELECT projects.category FROM projects;"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
